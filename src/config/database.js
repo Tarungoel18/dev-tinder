00e640bdb7a1564dfc769cbb29a088f188cdb1ab
Tarun Goel <tarungoel1823@gmail.com>
2025-01-28 21:14:53 +0530

    connect to db 2

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
+const { db_url } = require("../../constants");
 const dbConnect = async () => {
-  await mongoose.connect(
-    "mongodb+srv://tarungoel1823:WF9zA8PPeYFWSTYy@cluster0.ivvzh.mongodb.net/devTinder"
-  );
+  await mongoose.connect(db_url);
 };
 module.exports = {
   dbConnect,
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const { type } = require("express/lib/response");
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  Gender: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = {
+  User,
+};
