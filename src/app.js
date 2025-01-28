5a87cb2b26f9f82e4bac9382063b7960c17b0c09
Tarun Goel <tarungoel1823@gmail.com>
2025-01-28 23:03:25 +0530

    add some basic routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
 const express = require("express");
 const app = express();
 const { dbConnect } = require("./config/database");
+const { User } = require("./models/user");
+app.use(express.json());
+
+// app.use("/", (req, res, next) => {
+//   console.log(`Incoming request: ${req.method} ${req.url}`);
+//   next();
+// });
+
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("Successfully registered");
+  } catch (err) {
+    res.status(400).send("User not found");
+    console.log(err);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const users = await User.find({});
+  res.send(users);
+});
+
+app.get("/test", async (req, res) => {
+  const email = req.body.emailId;
+  const x = await User.find({ emailId: email });
+  if (x.length > 0) {
+    res.send(x);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
 dbConnect()
   .then(() => {
     console.log("Database successfully connected");
